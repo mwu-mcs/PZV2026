0e96d51f0a9176e170b51f4debafbece06b3b418
Markus Wulff <markus.wulff@mcs.ch>
2026-04-16 20:57:14 +0200

    units

--- modbus-be-strom/src/main.rs
+++ modbus-be-strom/src/main.rs
@@ -6,14 +6,14 @@ mod modbus_reader;
 async fn main() -> anyhow::Result<()> {
     let data = read_current("10.234.1.82:502").await?;
 
-    println!("\nDevice:    {}\n", data.device);
-    println!("Voltage:   {:.3} V", data.voltage);
-    println!("Current 1: {:.3} A", data.current_1);
-    println!("Current 2: {:.3} A", data.current_2);
-    println!("Current 3: {:.3} A", data.current_3);
-    println!("Power 1:   {:.3} W", data.power_1);
-    println!("Power 2:   {:.3} W", data.power_2);
-    println!("Power 3:   {:.3} W", data.power_3);
+    println!("\nDevice: {}\n", data.device);
+    println!("Voltage:   {}", data.voltage);
+    println!("Current 1: {}", data.current_1);
+    println!("Current 2: {}", data.current_2);
+    println!("Current 3: {}", data.current_3);
+    println!("Power 1:   {}", data.power_1);
+    println!("Power 2:   {}", data.power_2);
+    println!("Power 3:   {}", data.power_3);
 
     Ok(())
 }
--- modbus-be-strom/src/modbus_reader.rs
+++ modbus-be-strom/src/modbus_reader.rs
@@ -1,16 +1,29 @@
 use serde::Serialize;
+use std::fmt::Display;
 use tokio_modbus::client::{Client, Reader, tcp};
+
+#[derive(Debug, Serialize)]
+pub struct Qty {
+    value: f64,
+    unit: String,
+}
+
+impl Display for Qty {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:.3} {}", self.value, self.unit)
+    }
+}
 
 #[derive(Debug, Serialize)]
 pub struct DeviceData {
     pub device: String,
-    pub voltage: f64,
-    pub current_1: f64,
-    pub current_2: f64,
-    pub current_3: f64,
-    pub power_1: f64,
-    pub power_2: f64,
-    pub power_3: f64,
+    pub voltage: Qty,
+    pub current_1: Qty,
+    pub current_2: Qty,
+    pub current_3: Qty,
+    pub power_1: Qty,
+    pub power_2: Qty,
+    pub power_3: Qty,
 }
 
 // Common divisor for Voltage and Power
@@ -35,6 +48,7 @@ pub async fn read_current(address: &str) -> anyhow::Result<DeviceData> {
 
     println!("Fetching the current values ...");
     let raw_data = ctx.read_input_registers(0x0000, 7).await??;
+
     let voltage = f64::from(raw_data[0]) / DIVISOR_V_P;
     let current_1 = f64::from(raw_data[1]) / DIVISOR_I;
     let current_2 = f64::from(raw_data[2]) / DIVISOR_I;
@@ -45,13 +59,34 @@ pub async fn read_current(address: &str) -> anyhow::Result<DeviceData> {
 
     let be_strom = DeviceData {
         device,
-        voltage,
-        current_1,
-        current_2,
-        current_3,
-        power_1,
-        power_2,
-        power_3,
+        voltage: Qty {
+            value: voltage,
+            unit: "V".to_string(),
+        },
+        current_1: Qty {
+            value: current_1,
+            unit: "A".to_string(),
+        },
+        current_2: Qty {
+            value: current_2,
+            unit: "A".to_string(),
+        },
+        current_3: Qty {
+            value: current_3,
+            unit: "A".to_string(),
+        },
+        power_1: Qty {
+            value: power_1,
+            unit: "W".to_string(),
+        },
+        power_2: Qty {
+            value: power_2,
+            unit: "W".to_string(),
+        },
+        power_3: Qty {
+            value: power_3,
+            unit: "W".to_string(),
+        },
     };
 
     println!("Disconnecting");
